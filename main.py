19ce5a205ccf32d9b4301db665f285f800494e19
Ron Miller <ron@cloudswarm.net>
2023-07-10 19:48:47 -0700

    Logging and bugfixes

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import logging
 import os
+import sys
 from datetime import datetime
 
+import mastodon
 from dateutil.tz import tzutc
 from mako.template import Template
-import mastodon
+from pythonjsonlogger import jsonlogger
 
-MAX_DAYS = int(os.getenv('MAX_ACCOUNT_AGE', 30))  # Maximum age of accounts in days which will be considered
-RESULT_LIMIT = int(os.getenv('API_RESULT_LIMIT', 200))  # The mastodon API seems to ignore values higher than this
+
+log = logging.getLogger()
+log.setLevel(os.getenv("LOG_LEVEL", "INFO").upper())
+log_handler = logging.StreamHandler(sys.stdout)
+log_handler.setFormatter(jsonlogger.JsonFormatter())
+log.addHandler(log_handler)
 
 
 class Greeter:
@@ -18,17 +25,21 @@ class Greeter:
             api_base_url=os.getenv('MASTODON_BASE_URL'),
             user_agent='mastogreeter'
         )
+        self._max_days = int(os.getenv('MAX_ACCOUNT_AGE', 30))  # Maximum age of accounts in days which will be considered
+        self._max_to_greet = int(os.getenv('MAX_GREETINGS_PER_RUN', 0))  # Limit how many greetings you'll send in a single run
+        self._result_limit = int(os.getenv('API_RESULT_LIMIT', 200))  # The mastodon API seems to ignore values higher than this
         self._utcnow = datetime.utcnow().replace(tzinfo=tzutc())
         self._tpl = Template(filename="message.txt")
         self._instance_data = None
         self._contacted_list_id_value = None
         self._contacted = None
         self._active_user_ids = None
+        self._log = self._get_logger()
 
     @property
     def contacted(self):
         if self._contacted is None:
-            first_page = self._svc.conversations(limit=RESULT_LIMIT)
+            first_page = self._svc.conversations(limit=self._result_limit)
             self._contacted = set(
                 [
                     acct['id'] for c in first_page
@@ -36,19 +47,20 @@ class Greeter:
                     if self.not_too_old(c['last_status']['created_at'])
                 ]
             )
-            next_page = self._svc.fetch_next(first_page) if self.not_too_old(first_page[-1]['last_status']['created_at']) else None
-            while next_page is not None:
-                self._contacted.add(
-                    [
-                        acct['id'] for c in next_page
-                        for acct in c['accounts']
-                        if self.not_too_old(c['last_status']['created_at'])
-                    ]
-                )
-                if self.too_old(next_page[-1]['last_status']['created_at']):
-                    break
-                else:
-                    next_page = self._svc.fetch_next(next_page)
+            if len(first_page) > 0:
+                next_page = self._svc.fetch_next(first_page) if self.not_too_old(first_page[-1]['last_status']['created_at']) else None
+                while next_page is not None:
+                    self._contacted.update(
+                        [
+                            acct['id'] for c in next_page
+                            for acct in c['accounts']
+                            if self.not_too_old(c['last_status']['created_at'])
+                        ]
+                    )
+                    if self.too_old(next_page[-1]['last_status']['created_at']):
+                        break
+                    else:
+                        next_page = self._svc.fetch_next(next_page)
         return self._contacted
 
     @property
@@ -64,27 +76,30 @@ class Greeter:
             first_page = self._svc.admin_accounts_v2(
                 origin="local",
                 status="active",
-                limit=RESULT_LIMIT
+                limit=self._result_limit
             )
+            self._log.debug(f"Found {len(first_page)} active users in this page")
             self._active_user_ids = set(
                 [
                     x['id'] for x in first_page
                     if self.not_too_old(x['created_at'])
                 ]
             )
-            next_page = self._svc.fetch_next(first_page) if self.not_too_old(first_page[-1]['created_at']) else None
-
-            while next_page is not None:
-                self._active_user_ids.add(
-                    [
-                        x['id'] for x in first_page
-                        if self.not_too_old(x['created_at'])
-                    ]
-                )
-                if self.too_old(next_page[-1]['created_at']):
-                    break
-                else:
-                    next_page = self._svc.fetch_next(next_page)
+            self._log.debug(f"Now have {len(self._active_user_ids)} known active users")
+            if len(first_page) > 0:
+                next_page = self._svc.fetch_next(first_page) if self.not_too_old(first_page[-1]['created_at']) else None
+                while next_page is not None:
+                    self._active_user_ids.update(
+                        [
+                            x['id'] for x in next_page
+                            if self.not_too_old(x['created_at'])
+                        ]
+                    )
+                    self._log.debug(f"Now have {len(self._active_user_ids)} known active users")
+                    if self.too_old(next_page[-1]['created_at']):
+                        break
+                    else:
+                        next_page = self._svc.fetch_next(next_page)
 
         return self._active_user_ids
 
@@ -92,8 +107,15 @@ class Greeter:
         return self.active_user_ids.difference(self.contacted)
 
     def greet_users(self, user_ids):
+        user_ids = sorted(user_ids)  # Always sort in ascending order so those who've waited the longest get greeted first
+        greeted = 0
         for id in user_ids:
             if id not in self.contacted:
+                if self._max_to_greet > 0 and greeted >= self._max_to_greet:
+                    break
+                else:
+                    greeted += 1
+
                 account = self._svc.admin_account(id)
                 message = self._tpl.render(
                     username=account['username'],
@@ -102,21 +124,35 @@ class Greeter:
                     donation_link=os.getenv('DONATION_LINK')
                 )
                 self._contacted.add(id)
+                self._log.info(f"Sending greeting to @{account['username']}@{self.instance_data['uri']}")
                 self._svc.status_post(
                     status=message,
                     visibility="direct"
                 )
+        self._log.info(f"Sent greetings to {greeted} user(s)")
 
     def too_old(self, timestamp):
-        return (self._utcnow - timestamp).days > MAX_DAYS
+        return (self._utcnow - timestamp).days > self._max_days
 
     def not_too_old(self, timestamp):
         return not self.too_old(timestamp)
+
+    def _get_logger(self):
+        if 'log' not in globals():
+            log = logging.getLogger()
+            log.setLevel(os.getenv("LOG_LEVEL", "INFO").upper())
+            log_handler = logging.StreamHandler(sys.stdout)
+            log_handler.setFormatter(jsonlogger.JsonFormatter())
+            log.addHandler(log_handler)
+            return log
+        else:
+            return globals()['log']
 
 
 def main():
     greeter = Greeter()
     users_to_greet = greeter.get_users_to_greet()
+    log.info(f"Found {len(users_to_greet)} active user(s) to greet")
     greeter.greet_users(users_to_greet)
 
 
